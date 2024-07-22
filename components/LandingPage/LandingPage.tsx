5913b8b1a61a0e86088a262d0a1302507dc65b87
SeniorDev <tech.eng.me@gmail.com>
2024-07-22 16:27:57 -0400

    I add photo in the center of landing page

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+
 const LandingPage = () => (
-  <div className="bg-center bg-cover bg-[url('/images/home.jpg')] w-full h-full bg-center" />
+  <div
+    className="bg-center bg-cover bg-[url('/images/home.jpg')] bg-center
+  flex justify-center items-center w-full h-full"
+  >
+    <div className="relative h-full aspect-[836/870]">
+      <Image
+        src="/images/space-station.png"
+        alt=""
+        className="absolute left-0 top-0 size-full"
+        layout="fill"
+      />
+    </div>
+  </div>
 );
 
 export default LandingPage;
