21059498f31dd3aadeb7f02e67da99dee62c5408
SeniorDev <tech.eng.me@gmail.com>
2024-07-23 08:03:18 -0400

    I complete tooltip

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -3,12 +3,21 @@
 import ImageMapper from 'react-img-mapper'
 import map from '@/lib/image-map.json'
 import { useMeasure } from 'react-use'
+import { useState } from 'react'
 
 const LandingPage = () => {
   const [containerRef, { height }] = useMeasure() as any
+  const [isVisibleToolTip, setIsVisibleTooltip] = useState(false)
+  const [tooltipX, setTooltipX] = useState(0)
+  const [tooltipY, setTooltipY] = useState(0)
 
-  const showTooltip = () => {
-    console.log('ZIAD HERE')
+  const showTooltip = (area: any, index: any, e: any) => {
+    setIsVisibleTooltip(true)
+    const x = e.clientX
+    const y = e.clientY
+
+    setTooltipX(x)
+    setTooltipY(y)
   }
 
   return (
@@ -17,13 +26,28 @@ const LandingPage = () => {
       w-full h-full flex items-center justify-center"
       ref={containerRef}
     >
-      <ImageMapper
-        src="/images/space-station.png"
-        map={map}
-        responsive
-        parentWidth={(height / 870) * 836}
-        onMouseMove={showTooltip}
-      />
+      <div className="cursor-pointer">
+        <ImageMapper
+          src="/images/space-station.png"
+          map={map}
+          responsive
+          parentWidth={(height / 870) * 836}
+          onMouseMove={(area, index, e) => showTooltip(area, index, e)}
+          onMouseLeave={() => setIsVisibleTooltip(false)}
+        />
+      </div>
+      {isVisibleToolTip && (
+        <div
+          className="bubble-name"
+          style={{
+            left: tooltipX,
+            top: tooltipY,
+          }}
+        >
+          El Niño Estrella is a multimedia experience. The smart album is a limited edition digital
+          box set
+        </div>
+      )}
     </div>
   )
 }
