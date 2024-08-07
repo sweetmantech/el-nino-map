159e46b0d7a458472d9139daeb9f533ac33157b0
SeniorDev <tech.eng.me@gmail.com>
2024-08-07 10:00:59 -0400

    I fix feedback

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -4,3 +4,4 @@ export const IS_TESTNET = process.env.NEXT_PUBLIC_TEST === 'true'
 export const CHAIN = IS_TESTNET ? baseSepolia : base
 export const CHAIN_ID = CHAIN.id
 export const FIRST_SMART_WALLET_LOGIN_EVENT = 'first_smart_wallet_login'
+export const SMART_WALLET_LOGIN_POINT = 11
--- lib/stack/trackLoginPoints.tsx
+++ lib/stack/trackLoginPoints.tsx
@@ -1,12 +1,12 @@
 import { Address } from 'viem'
 import getStackClient from './getStackClient'
-import { FIRST_SMART_WALLET_LOGIN_EVENT } from '../consts'
+import { FIRST_SMART_WALLET_LOGIN_EVENT, SMART_WALLET_LOGIN_POINT } from '../consts'
 
 const trackLoginPoints = async (address) => {
   const stackClient = getStackClient()
 
   await stackClient.track(FIRST_SMART_WALLET_LOGIN_EVENT, {
-    points: 10,
+    points: SMART_WALLET_LOGIN_POINT,
     account: address as Address,
     uniqueId: `${Date.now()}`,
   })
