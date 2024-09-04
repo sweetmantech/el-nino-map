751e6d1a2315f2071680a221e593192f42ab2e92
SeniorDev <tech.eng.me@gmail.com>
2024-09-04 15:17:51 -0400

    I fix chain

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -1,13 +1,11 @@
-import { base, baseSepolia } from 'thirdweb/chains'
+import { base } from 'thirdweb/chains'
 
 export const IS_TESTNET = process.env.NEXT_PUBLIC_TEST === 'true'
-export const CHAIN = IS_TESTNET ? baseSepolia : base
+export const CHAIN = base
 export const CHAIN_ID = CHAIN.id
 
 // Zora
-export const DROP_ADDRESS = IS_TESTNET
-  ? '0x0b93a56db47797142076e24c520c846c9bd0d6fa' // base sepolia
-  : '0x16F1FC98282AFDA367999012027b5A3fA656a713' // base mainnet
+export const DROP_ADDRESS = '0x16F1FC98282AFDA367999012027b5A3fA656a713'
 
 export const ZORA_PRICE = '777000000000000'
 
