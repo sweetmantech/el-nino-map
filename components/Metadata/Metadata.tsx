9d5b20e7b76bde50ab97f1bc78fa1f04c7cd0537
SeniorDev <tech.eng.me@gmail.com>
2024-10-25 17:28:54 -0400

    I add metdata text popup.

---
+++ components/Metadata/Metadata.tsx
@@ -0,0 +1,23 @@
+const Metadata = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <div
+      className="absolute left-0 top-0 w-full h-full z-[15]
+      flex items-center justify-center"
+      onClick={(e: any) => {
+        if (e.target === e.currentTarget) onClose()
+      }}
+    >
+      <div className="max-w-[300px] p-3 bg-white rounded-md" id="metadata">
+        <fieldset className="font-[600] text-lg">
+          <p>Artist: xcelencia</p>
+          <p>Album: el niño estrella</p>
+          <p>Designer: muchozorro</p>
+          <p>Dev: sweetman</p>
+          <p>Producers: emme, shine, x</p>
+        </fieldset>
+      </div>
+    </div>
+  )
+}
+
+export default Metadata
