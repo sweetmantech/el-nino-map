085fe21304bdd149441f2d7414a7b51e26b22878
❤️ onchain ❤️ <tech.eng.me@gmail.com>
2024-11-23 15:26:08 -0800

    I remove dev logs

--- lib/getCollectors.tsx
+++ lib/getCollectors.tsx
@@ -3,20 +3,18 @@ const getCollectors = (data) => {
   const usersSet = new Set()
 
   data.forEach((item) => {
-    const tokenId = item.args.tokenId.toString() // Convert BigInt to string for use as a key
+    const tokenId = item.args.tokenId.toString()
     const user = item.args.user
 
-    // If the tokenId doesn't exist in the aggregated object, create it
     if (!aggregated[tokenId]) {
       aggregated[tokenId] = {
-        address: item.address, // Assuming address is the same for each tokenId
+        address: item.address,
         permissions: item.args.permissions,
         tokenId: tokenId,
         users: [],
       }
     }
 
-    // Push the user into the array, avoiding duplicates
     if (!aggregated[tokenId].users.includes(user)) {
       aggregated[tokenId].users.push(user)
     }
