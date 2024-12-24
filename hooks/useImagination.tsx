d121c2ef5b995fd8cb2775aff1ef0b28b3bb61d7
Va~~~dym <tech.eng.me@gmail.com>
2024-12-24 11:19:08 -0800

    fix: points

--- hooks/useImagination.tsx
+++ hooks/useImagination.tsx
@@ -1,21 +1,16 @@
 import getMemoriesPoints from '@/lib/stack/getMemoriesPoints'
 import { useEffect, useState } from 'react'
-import { useActiveAccount } from 'thirdweb/react'
-import { Account } from 'thirdweb/wallets'
 
 const useImagination = () => {
   const [events, setEvents] = useState([])
-  const activeAccount: Account = useActiveAccount()
-  const address = activeAccount?.address
 
   useEffect(() => {
     const init = async () => {
       const points = await getMemoriesPoints()
       setEvents(points)
     }
-    if (!address) return
     init()
-  }, [address])
+  }, [])
 
   return {
     events,
