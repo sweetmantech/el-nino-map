cc34e5c37667147421056c7b3d55035e01b5fad3
Va~~~dym <tech.eng.me@gmail.com>
2024-12-24 05:10:50 -0800

    add: gap

--- components/ImaginationPage/ImaginationPage.tsx
+++ components/ImaginationPage/ImaginationPage.tsx
@@ -6,7 +6,7 @@ const ImaginationPage = () => {
   const { events } = useImagination()
 
   return (
-    <div className="w-full grid grid-cols-3 mx-auto max-w-3xl pt-20">
+    <div className="w-full grid grid-cols-3 mx-auto max-w-3xl pt-20 gap-2">
       {events.map((event) => (
         <div key={event.metadata.uniqueId} className="border-[2px] rounded-md p-2 border-black">
           <p>POST URL: {event.metadata.url}</p>
