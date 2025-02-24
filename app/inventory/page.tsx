fe3d7969549cf4653813bdec22a124b0302f87a6
dev <tech.eng.me@gmail.com>
2025-02-24 07:01:03 -0800

    add: inventory page

---
+++ app/inventory/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import InventoryPage from "@/components/InventoryPage";
+
+const Inventory = () => <InventoryPage />
+
+export default Inventory
---
+++ components/InventoryPage/InventoryPage.tsx
@@ -0,0 +1,10 @@
+
+const InventoryPage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      
+    </div>
+  );
+};
+
+export default InventoryPage;
