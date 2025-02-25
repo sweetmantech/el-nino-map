409319c68481c81b3aa0ddf81f704e17b6c160f4
dev <tech.eng.me@gmail.com>
2025-02-25 09:07:15 -0800

    add card

--- components/InventoryPage/InventoryPage.tsx
+++ components/InventoryPage/InventoryPage.tsx
@@ -1,12 +1,13 @@
 import usePosts, { ZoraPost } from '@/hooks/usePosts'
 import { Skeleton } from '../ui/skeleton'
 import Image from 'next/image'
+import { Card, CardContent, CardFooter } from '../ui/card'
 
 const InventoryPage = () => {
   const { data: posts, isLoading } = usePosts()
 
   return (
-    <main className="container grid grid-cols-3 gap-4 min-h-screen gap-10 p-20">
+    <main className="container grid grid-cols-3 gap-4 min-h-screen py-24">
       {isLoading ? (
         <>
           {Array.from({ length: 9 }).map((_, i) => (
@@ -16,23 +17,23 @@ const InventoryPage = () => {
       ) : (
         <>
           {posts.map((p: ZoraPost, i) => (
-            <div
-              className="col-span-1 aspect-1/1 flex flex-col items-center 
-              border-[2px] border-black overflow-hidden rounded-md bg-black"
-              key={i}
-            >
-              <div className="grow w-full relative overflow-hidden">
-                <Image
-                  src={p.preview}
-                  layout="fill"
-                  alt="not found preview"
-                  objectFit="cover"
-                  objectPosition="center"
-                  blurDataURL={p.blurhash}
-                />
-              </div>
-              <p className="font-titilliumweb text-left text-white">{p.name}</p>
-            </div>
+            <Card className="bg-slate-900" key={i}>
+              <CardContent className="p-3">
+                <div className="aspect-video relative bg-slate-800 rounded-lg flex items-center justify-center">
+                  <Image
+                    src={p.preview}
+                    layout="fill"
+                    alt="not found preview"
+                    objectFit="cover"
+                    objectPosition="center"
+                    blurDataURL={p.blurhash}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter className="!p-3">
+                <p className="font-titilliumweb text-left text-white">{p.name}</p>
+              </CardFooter>
+            </Card>
           ))}
         </>
       )}
