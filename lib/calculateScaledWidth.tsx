5a1df6719e0aef0e3360b90ae784d8083966ffff
dev <tech.eng.me@gmail.com>
2025-03-03 07:01:19 -0800

    feat: pulsating

--- lib/calculateScaledWidth.tsx
+++
@@ -1,7 +0,0 @@
-function calculateScaledWidth(width, height) {
-  const aspectRatio = 8000 / 4500 // Pre-calculate for readability
-  const scaledWidth = width / height > aspectRatio ? width : height * aspectRatio
-  return scaledWidth
-}
-
-export default calculateScaledWidth
