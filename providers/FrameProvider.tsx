4529f111243eb3a81b5e963aa0801f35958ff927
dev <tech.eng.me@gmail.com>
2025-04-03 09:50:43 -0700

    fix frame provider

--- providers/FrameProvider.tsx
+++ providers/FrameProvider.tsx
@@ -2,21 +2,21 @@
 
 // eslint-disable-next-line import/no-named-as-default
 import sdk from '@farcaster/frame-sdk'
-import { useEffect, useState, ReactNode } from 'react'
+import React, { useState, useEffect, ReactNode } from 'react'
 
 export default function FrameProvider({ children }: { children: ReactNode }) {
-  const [isSDKLoaded, setIsSDKLoaded] = useState(false)
+  const [isSDKLoaded, setIsSDKLoaded] = useState<boolean>(false)
 
   useEffect(() => {
     const load = async () => {
       sdk.actions.ready()
     }
+
     if (sdk && !isSDKLoaded) {
       setIsSDKLoaded(true)
       load()
     }
-    // eslint-disable-next-line
   }, [isSDKLoaded])
 
-  return <div>{children}</div>
+  return <React.Fragment>{children}</React.Fragment>
 }
