8aca42074d9247069dc6f7e4dc697a920304ab5e
dev <tech.eng.me@gmail.com>
2025-04-16 09:54:56 -0700

    fix: back to home

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,18 +5,36 @@ import { ConnectButton } from 'thirdweb/react'
 import { base, sepolia } from 'thirdweb/chains'
 import { wallets } from '@/lib/thirdweb/wallets'
 import { IS_TESTNET } from '@/lib/consts'
+import { usePathname, useRouter } from 'next/navigation'
 
-const Header = () => (
-  <div className="fixed z-[100] top-4 right-8 w-fit">
-    <ConnectButton
-      client={client}
-      wallets={wallets}
-      chain={IS_TESTNET ? sepolia : base}
-      connectButton={{
-        label: 'EXPLORE',
-      }}
-    />
-  </div>
-)
+const Header = () => {
+  const pathname = usePathname()
+  const isHomePage = pathname === '/'
+  const { push } = useRouter()
+
+  return (
+    <div className="fixed z-[100] top-4 left-0 px-8 w-fit flex justify-between items-center w-screen">
+      {!isHomePage && (
+        <button
+          type="button"
+          className="bg-black text-white px-3 py-2 font-titilliumweb rounded-md"
+          onClick={() => push('/')}
+        >
+          Back To Home
+        </button>
+      )}
+      <div className="ml-auto">
+        <ConnectButton
+          client={client}
+          wallets={wallets}
+          chain={IS_TESTNET ? sepolia : base}
+          connectButton={{
+            label: 'EXPLORE',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
 
 export default Header
