ffd937bf33aba01fa4e07b8a49656d140db4f510
dev <tech.eng.me@gmail.com>
2025-04-16 05:32:39 -0700

    feat: warpcast mini app assets

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ const TITLE = 'El Niño Maravilla Pt. 1'
 const DESCRIPTION = `El Niño Maravilla is the debut album by xcelencia, showcasing a unique blend of Latin
 urban and pop sounds. This project brings together a talented team of designers,
 developers, and producers to create a groundbreaking musical experience.`
-const IMAGE = 'https://arweave.net/M6e7WX1fsQwvysE4aWimystlkb6havTwS9B4Mb8s6xU'
 
 export const revalidate = 300
 
@@ -18,12 +17,12 @@ export const metadata: Metadata = {
   openGraph: {
     title: TITLE,
     description: DESCRIPTION,
-    images: [IMAGE],
+    images: ['https://el-nino-map.vercel.app/favicon.png'],
   },
   other: {
     'fc:frame': JSON.stringify({
       version: 'next',
-      imageUrl: IMAGE,
+      imageUrl: 'https://el-nino-map.vercel.app/favicon.png',
       aspectRatio: '1:1',
       button: {
         title: 'Collect',
@@ -31,8 +30,8 @@ export const metadata: Metadata = {
           type: 'launch_frame',
           name: TITLE,
           url: 'https://el-nino-map.vercel.app',
-          iconImageUrl: IMAGE,
-          splashImageUrl: IMAGE,
+          iconImageUrl: 'https://el-nino-map.vercel.app/miniapp-icon.png',
+          splashImageUrl: 'https://el-nino-map.vercel.app/miniapp-cover.jpeg',
           splashBackgroundColor: '#6330d0',
         },
       },
