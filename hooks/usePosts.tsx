f5c5f5d553576a0169a50da264ee4749349befa2
dev <tech.eng.me@gmail.com>
2025-04-16 09:37:52 -0700

    aggregated manifold

--- hooks/usePosts.tsx
+++ hooks/usePosts.tsx
@@ -1,28 +1,16 @@
+import getManifoldPosts from '@/lib/manifold/getManifoldPosts'
 import { CollectionMetadata, getMetadata } from '@/lib/viem/getMetadata'
-import getBalances from '@/lib/viem/getBalances'
+import getZoraPosts from '@/lib/zora/getZoraPosts'
 import { useQuery } from '@tanstack/react-query'
 import { useActiveAccount } from 'thirdweb/react'
 import { Address } from 'viem'
 
-export type CreatedContract = {
-  newContract: Address
-  chainId: number
-  name: string
-  contractURI: string
-  nextTokenId: bigint
-  batchBalances: bigint[]
-}
-
 async function fetchPosts(address: Address | undefined): Promise<CollectionMetadata[]> {
   try {
     if (!address) return []
-    const response = await fetch(`/api/posts/zora`)
-    const zoraCreatedContracts = await response.json()
-    const collectionsAndBalances = await getBalances(address, zoraCreatedContracts)
-    const filtered = collectionsAndBalances.filter((c) =>
-      c.batchBalances.flat().some((b) => BigInt(b) > BigInt(0)),
-    )
-    const metadata = await getMetadata(filtered)
+    const zoraCreatedPosts = await getZoraPosts(address)
+    const manifoldCreatedPosts = await getManifoldPosts(address)
+    const metadata = await getMetadata([...zoraCreatedPosts, ...manifoldCreatedPosts])
     return metadata
   } catch (error) {
     throw new Error('Failed to fetch zora posts.')
