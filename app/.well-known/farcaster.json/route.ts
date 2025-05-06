93fb19c5968948702c00d21992cb0ed9f017df6d
dev <tech.eng.me@gmail.com>
2025-05-06 14:41:34 -0700

    fix route

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,24 @@
+import { APP_URL } from '../../../lib/farcaster/consts'
+
+export async function GET() {
+  const config = {
+    accountAssociation: {
+      header: process.env.FARCASTER_HEADER,
+      payload: process.env.FARCASTER_PAYLOAD,
+      signature: process.env.FARCASTER_SIGNATURE,
+    },
+    frame: {
+      version: '1',
+      name: 'Example Frame',
+      iconUrl: `${APP_URL}/icon.png`,
+      homeUrl: APP_URL,
+      imageUrl: `${APP_URL}/image.png`,
+      buttonTitle: 'Check this out',
+      splashImageUrl: `${APP_URL}/splash.png`,
+      splashBackgroundColor: '#eeccff',
+      webhookUrl: `${APP_URL}/api/webhook`,
+    },
+  }
+
+  return Response.json(config)
+}
