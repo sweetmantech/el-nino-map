c65cd91f6a6b9b45b48f9829d01ea18d1a6be88e
Sweets Sweetman <sweetmantech@gmail.com>
2025-06-26 23:41:14 -0300

    preview url

--- components/ChatPage/ChatPage.tsx
+++ components/ChatPage/ChatPage.tsx
@@ -6,16 +6,18 @@ import MessageInput from './MessageInput'
 import ChatHeader from './ChatHeader'
 
 const ChatPage = () => {
-  const { messages, input, setInput, handleSubmit, isLoading } = useChat()
+  const { messages, input, setInput, handleSubmit, isLoading } = useChat({
+    api: 'https://recoup-chat-git-cursor-remove-cors-b-6636cd-recoupable-ad724970.vercel.app/api/chat',
+  })
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <ChatHeader />
       <div className="flex-1 max-w-2xl mx-auto w-full flex flex-col">
         <MessageList messages={messages} />
-        <MessageInput 
-          value={input} 
-          onChange={setInput} 
+        <MessageInput
+          value={input}
+          onChange={setInput}
           onSubmit={handleSubmit}
           isLoading={isLoading}
         />
