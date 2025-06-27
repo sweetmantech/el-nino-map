0372b5e427cd124e396ada9a5ef4195d5cfd7efc
Sweets Sweetman <sweetmantech@gmail.com>
2025-06-26 23:56:56 -0300

    I add artistId and accountID

--- components/ChatPage/ChatPage.tsx
+++ components/ChatPage/ChatPage.tsx
@@ -8,6 +8,10 @@ import ChatHeader from './ChatHeader'
 const ChatPage = () => {
   const { messages, input, setInput, handleSubmit, isLoading } = useChat({
     api: 'https://chat.recoupable.com/api/chat',
+    body: {
+      artistId: 'eaa2fb07-5a4b-4710-9c0d-4a74db3612d2',
+      accountId: '46cd41de-88a8-4839-b03b-264a8566cccf',
+    },
   })
 
   return (
