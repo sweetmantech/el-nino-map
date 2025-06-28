24cc43db388d44e8d9c4a9dfcc67b094ddb59074
Sweets Sweetman <sweetmantech@gmail.com>
2025-06-28 19:51:27 -0300

    Rollback changes to Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -14,24 +14,14 @@ const Modal = ({
       if (e.key === 'Escape') onClose()
     }
 
-    if (open) {
-      // Prevent body scroll when modal is open
-      document.body.style.overflow = 'hidden'
-      window.addEventListener('keydown', handleEscapeEvent)
-    }
-
-    return () => {
-      // Restore body scroll when modal closes
-      document.body.style.overflow = 'unset'
-      window.removeEventListener('keydown', handleEscapeEvent)
-    }
+    window.addEventListener('keydown', handleEscapeEvent)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open])
+  }, [])
 
   if (!open) return <Fragment />
 
   return (
-    <div className="fixed left-0 top-0 z-[100] w-screen h-screen bg-black bg-opacity-50">
+    <div className="fixed left-0 top-0 z-[100] w-screen h-screen">
       <div
         className="absolute left-0 top-0 w-full h-full z-[15]
               flex items-center justify-center"
