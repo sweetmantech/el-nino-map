2aac5bf1c25acf8a4ef4284df437ed5c3b377117
Sweets Sweetman <sweetmantech@gmail.com>
2025-08-15 14:58:45 -0500

    Subscribe popup - WhatsApp Group

--- components/Hypersub/Hypersub.tsx
+++ components/Hypersub/Hypersub.tsx
@@ -1,33 +1,9 @@
 import Modal from '../Modal'
-import Image from 'next/image'
-import { useSubscriptionInfoProvider } from '@/providers/SubscriptionProvider'
-import useSubscribe from '@/hooks/useSubscribe'
-import { formatUnits } from 'viem'
-import FiatSubscribeModal from './FiatSubscribeModal'
-import { useMapProvider } from '@/providers/MapProvider'
 
 const Hypersub = ({ onClose }: { onClose: () => void }) => {
-  const { pricePerPeriod, symbol, decimals } = useSubscriptionInfoProvider()
-  const { subscribe, loading, photos, subscribed, fiatActive, setFiatActive } = useSubscribe()
-  const { setIsHypersubOpen } = useMapProvider()
-
   const handleClick = async () => {
-    if (subscribed) {
-      window.open('https://hypersub.xyz/s/enm-lb6jxqtldv5s', '_blank')
-      return
-    }
-    await subscribe()
+    window.open('https://chat.whatsapp.com/Cm5uQ5MBnx00ILGAS3OE0S', '_blank')
   }
-
-  if (fiatActive)
-    return (
-      <FiatSubscribeModal
-        onClose={() => {
-          setIsHypersubOpen(false)
-          setFiatActive(false)
-        }}
-      />
-    )
 
   return (
     <Modal onClose={onClose}>
@@ -35,29 +11,17 @@ const Hypersub = ({ onClose }: { onClose: () => void }) => {
         className="max-w-[400px] px-6 py-2 bg-white rounded-md flex-col flex gap-2"
         id="subscribe"
       >
-        <p className="text-xl font-bold text-center font-italipixel text-xl">Subscription</p>
-        <div className="flex gap-2 justify-center">
-          {photos.map((photo) => (
-            <div
-              className="w-[100px] aspect-[1/1] relative z-[10] border-[2px] border-grey-light rounded-full overflow-hidden"
-              key={photo}
-            >
-              <Image src={photo} layout="fill" className="absolute object-cover" alt="" />
-            </div>
-          ))}
-        </div>
+        <p className="text-xl font-bold text-center font-italipixel text-xl">👀 Tap in</p>
         <p className="font-titilliumweb">
-          {subscribed
-            ? `To stay up to date with your subscription, visit the hypersub`
-            : `Subscribe to ENM by La Equis ${formatUnits(pricePerPeriod, decimals)} ${symbol}/month for exclusive content, early access & surprise drops.`}
+          Únete al WhatsApp — es la vibra de Maravilla City. Música, stories, el proceso y conexión
+          directa conmigo.
         </p>
         <button
           type="button"
           className="w-fit px-3 mx-auto my-1 border-[1px] border-grey rounded-md font-semibold py-1 font-titilliumweb"
           onClick={handleClick}
-          disabled={loading}
         >
-          {loading ? 'Subscribing...' : <>{subscribed ? 'View' : 'Subscribe'}</>}
+          Subscribe
         </button>
       </div>
     </Modal>
