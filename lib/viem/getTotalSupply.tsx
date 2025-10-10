d388d271331659f5b76b9d9b2b896f000d51df90
Sweets Sweetman <sweetmantech@gmail.com>
2025-10-10 15:47:47 -0500

    getTotalSupply - build errors.

--- lib/viem/getTotalSupply.tsx
+++ lib/viem/getTotalSupply.tsx
@@ -1,13 +1,14 @@
 import { erc721Abi } from 'viem'
 import { getPublicClient } from '../clients'
 import { CHAIN_ID } from '../consts'
+import { type Address } from 'viem'
 
 const getTotalSupply = async (dropAddress) => {
   const response = await getPublicClient(CHAIN_ID).readContract({
-    address: dropAddress as `0x${string}`,
+    address: dropAddress as Address,
     abi: erc721Abi,
     functionName: 'totalSupply',
-  })
+  } as any)
   return response
 }
 
