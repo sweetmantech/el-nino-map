e751c4c15abea79c2b1ea8dd1ca81e53997861ca
Sweets Sweetman <sweetmantech@gmail.com>
2025-10-10 13:41:49 -0500

    use client

--- components/StripePage/StripeCheckout.tsx
+++ components/StripePage/StripeCheckout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useEffect, useState } from 'react'
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js'
 import { getStripe } from '@/lib/stripe/client'
