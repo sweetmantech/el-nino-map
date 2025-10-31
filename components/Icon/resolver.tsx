6f74e8614dbf129ff74c004ba40f3db024968db9
thomas jin <tech.eng.me@gmail.com>
2025-11-01 01:19:47 +1000

    maravilla pass preview modal

--- components/Icon/resolver.tsx
+++ components/Icon/resolver.tsx
@@ -4,4 +4,9 @@ export const Icons = {
       <path d="M11 0L8 8L0 11L8 14L11 22L14 14L22 11L14 8L11 0Z" fill="black" />
     </svg>
   ),
+  'white-star': () => (
+    <svg width="16" height="16" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11 0L8 8L0 11L8 14L11 22L14 14L22 11L14 8L11 0Z" fill="white" />
+    </svg>
+  ),
 }
