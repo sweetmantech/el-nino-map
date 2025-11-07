e1e0b7db0262e94d919469a7dc2334dc8aeeccb2
thomas jin <tech.eng.me@gmail.com>
2025-11-07 13:56:19 +1000

    fix embed script

--- components/InventoryPage/InvertoryZora.tsx
+++ components/InventoryPage/InvertoryZora.tsx
@@ -1,12 +1,16 @@
-import useIframely from '@/hooks/useIframely'
 import { Skeleton } from '@/components/ui/skeleton'
+import Script from 'next/script'
+import { useState } from 'react'
 
 const InventoryZora = () => {
-  const { isLoaded } = useIframely()
+  const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <div className="bg-slate-900 rounded-xl p-6 h-fit">
       <h3 className="text-xl font-bold text-white mb-4">Maravilla on Zora</h3>
+      <Script async src="https://iframely.net/embed.js" onLoad={() => {
+        setIsLoaded(true)
+      }} />
       {isLoaded ? (
         <div className="iframely-embed">
           <div className="iframely-responsive !pb-[49%] pt-[120px]">
