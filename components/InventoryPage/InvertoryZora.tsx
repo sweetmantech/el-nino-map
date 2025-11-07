6b9a452aff108166bf23d928b7709f1ff4562208
thomas jin <tech.eng.me@gmail.com>
2025-11-07 14:30:24 +1000

    fix zora embedd

--- components/InventoryPage/InvertoryZora.tsx
+++ components/InventoryPage/InvertoryZora.tsx
@@ -1,28 +1,19 @@
-import { Skeleton } from '@/components/ui/skeleton'
-import Script from 'next/script'
-import { useState } from 'react'
+import useIframely from '@/hooks/useIframely'
 
 const InventoryZora = () => {
-  const [isLoaded, setIsLoaded] = useState(false)
+  useIframely()
 
   return (
     <div className="bg-slate-900 rounded-xl p-6 h-fit">
       <h3 className="text-xl font-bold text-white mb-4">Maravilla on Zora</h3>
-      <Script async src="https://iframely.net/embed.js" onLoad={() => {
-        setIsLoaded(true)
-      }} />
-      {isLoaded ? (
-        <div className="iframely-embed">
-          <div className="iframely-responsive !pb-[49%] pt-[120px]">
-            <a
-              href="https://zora.co/@maravilla"
-              data-iframely-url="//iframely.net/H9BKs0Pq?theme=dark"
-            ></a>
-          </div>
+      <div className="iframely-embed">
+        <div className="iframely-responsive !pb-[49%] pt-[120px]">
+          <a
+            href="https://zora.co/@maravilla"
+            data-iframely-url="//iframely.net/H9BKs0Pq?theme=dark"
+          ></a>
         </div>
-      ) : (
-        <Skeleton className="w-full !pb-[48%] pt-[120px] rounded-lg bg-slate-800" />
-      )}
+      </div>
     </div>
   )
 }
